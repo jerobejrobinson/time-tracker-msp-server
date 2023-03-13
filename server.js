9bb9c64856612365f559dee074fbbc55a9990314
jerobe <jerobejrobinson@gmail.com>
2023-03-13 07:10:02 -0500

    saved changes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// require('dotenv').config()
 import express from 'express'
 import cors from 'cors'
 import fetch from 'node-fetch'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
@@ -8,43 +11,49 @@ app.use(cors({origin: "http://localhost:3000"}))
 
 
 app.get('/gabt', (req, res) => {
-    fetch(process.env.TOKEN_URL, {
+    fetch(process.env.url, {
         method: "POST",
-        body: `grant_type=${process.env.GRANT_TYPE}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&username=${process.env.USERNAME}&password=${process.env.PASSWORD}`,
+        body: ``,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-    }).then(res => res.json()).then(data => res.send(data))
+    }).then(res => res.json()).then(data => {
+      console.log(data)
+      res.send(data)
+    })
 })
-
+app.use(cors({origin: "http://localhost:3000"}))
 app.get('/api/getproductpricing', (req, res) => {
-    fetch(`${process.env.API_ENDPOINT}/sxapioepricingv5`, {
+    fetch(`https://mingle-ionapi.inforcloudsuite.com/D7NMH8MYY885DBPS_TRN/SX/rest/sxapirestservice/sxapioepricingv5`, {
             method: 'POST',
             headers: {
-                "Authorization": req.headers.authorization
+                "Authorization": req.headers.authorization,
+                "Content-Type": "application/json",
+                'Accept': 'application/json',
             },
-            body: JSON.stringify({
-                "request": {
+            body: `{
+              "request": {
                   "companyNumber": 3,
                   "operatorInit": "jjr",
                   "operatorPassword": "",
                   "customerNumber": 98762,
                   "warehouse": "300",
-                  "productCode": req.query.product,
-                  "quantity": 2,
+                  "productCode": "${req.query.product}",
+                  "quantity": ${req.query.quantity},
                   "tInfieldvalue": {
-                    "t-infieldvalue": [
-                      {
-                        "level": "string",
-                        "lineno": 0,
-                        "seqno": 0,
-                        "fieldname": "string",
-                        "fieldvalue": "string"
-                      }
-                    ]
+                      "t-infieldvalue": [
+                          {
+                          "level": "string",
+                          "lineno": 0,
+                          "seqno": 0,
+                          "fieldname": "string",
+                          "fieldvalue": "string"
+                          }
+                      ]
                   }
-                }
-              })
+              }         
+          }`
+            
         }).then(res => res.json()).then(data => res.send(data))
 })
 
